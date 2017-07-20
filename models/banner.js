6c11d0db91ca429698daa70bd70bfa0e0a46783e
Emir Mamashov <emir.mamashov@gmail.com>
2017-07-20 23:46:19 +0600

    add banner

---
+++ models/banner.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let bannaerSchema = new Schema({
+    name: { type: String, required: true },
+    photo: { type: Schema.Types.ObjectId, ref: 'Photo' }
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'update_at'
+    }
+});
+let Banner = mongoose.model('Banner', bannaerSchema);
+module.exports = (registry) => {
+    registry['Banner'] = Banner;
+}
