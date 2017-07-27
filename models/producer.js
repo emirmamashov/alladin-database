52f687db6742db0c9007aec2a987bfea378d0ff7
Emir Mamashov <emir.mamashov@gmail.com>
2017-07-27 23:12:44 +0600

    Добавление производителей

--- models/producer.js
+++ models/producer.js
@@ -5,7 +5,11 @@ let ProducerShema = new Schema({
     name: { type: String, required: true },
     description: { type: String },
     keywords: { type: String },
-    author: { type: String }
+    author: { type: String },
+    image: { type: String },
+    images: [
+        { type: String }
+    ]
 });
 
 let Producer = mongoose.model('Producer', ProducerShema);
